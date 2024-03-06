b3c10548f12534bb85fa59be9ffc2e0a597955b7
Joel Martin <joel@Joels-MacBook-Pro.local>
2024-03-06 23:05:42 +0000

    Avg attribute add

--- script.js
+++ script.js
@@ -4,7 +4,9 @@ console.log('Hello webflow!');
 
 const menu = document.getElementById('mobile-menu');
 const line3 = document.getElementById('line-3');
+const line2 = document.getElementById('line-2');
 
-menu.addEventListener('click', () => {
-    line3.setAttribute('width', '9.2986e-08');
+menu.addEventListener('mouseover', () => {
+    line3.setAttribute('x1', '9.2986e-08');
+    line2.setAttribute('x1', '9.2986e-08');
 });
