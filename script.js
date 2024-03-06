6ea1d96feed60297603c9ee49a809938476e443a
Joel Martin <joel@Joels-MacBook-Pro.local>
2024-03-06 22:50:20 +0000

    EventListener

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 console.log('Hello webflow!');
+
+//id='line-#'
+
+const menu = document.getElementById('mobile-menu');
+const line3 = document.getElementById('line-3');
+
+menu.addEventListener('click==', () => {
+    line3.style.width = '300px';
+});
